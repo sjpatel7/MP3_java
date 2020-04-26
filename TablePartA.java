af35f0e321efc37078447f7aecc466accd2a833c
sjpatel7 <31695897+sjpatel7@users.noreply.github.com>
2020-04-25 22:00:33 -0500

    first implementation

--- TablePartA.java
+++ TablePartA.java
@@ -20,9 +20,25 @@ import org.apache.hadoop.hbase.util.Bytes;
 public class TablePartA{
 
    public static void main(String[] args) throws IOException {
-
 	//TODO
-	
+	//create table named "powers" in HBase with 3 column families (personal, professional, custom)
+	//create table named "food" in HBase with 2 column families (nutrition, taste)
+	//treate all data as String
+	   
+	//instantiations
+	Configuration config = HBaseConfiguration.create();
+	HBaseAdmin admin = new HBaseAdmin(config);
+	HTableDescriptor tableDescriptor1 = new HTableDescriptor(TableName.valueOf("powers"));
+	HTableDescriptor tableDescriptor2 = new HTableDescriptor(TableName.valueOf("food")); 
+	//add column families
+	tableDescriptor1.addFamily(new HColumnDescriptor("personal"));
+	tableDescriptor1.addFamily(new HColumnDescriptor("professional"));
+	tableDescriptor1.addFamily(new HColumnDescriptor("custom"));   
+	tableDescriptor2.addFamily(new HColumnDescriptor("nutrition"));
+	tableDescriptor2.addFamily(new HColumnDescriptor("taste")); 
+	//Execute table through admin
+	admin.createTable(tableDescriptor1);
+	admin.createTable(tableDescriptor2);
    }
 }
 
