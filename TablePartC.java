85d587a4f2eadc59d8158c7ac28f42b0a9a0596f
sjpatel7 <31695897+sjpatel7@users.noreply.github.com>
2020-04-25 23:26:22 -0500

    first implementation

--- TablePartC.java
+++ TablePartC.java
@@ -25,7 +25,34 @@ public class TablePartC{
 
    public static void main(String[] args) throws IOException {
 
-	//TODO      
+	//TODO     
+	//insert data from input.csv into powers table created from part A
+	//schema is personal: hero, power ; professional: name, xp ; custom:  color
+	Configuration config = HBaseConfiguration.create();
+	HTable hTable = new HTable(config, "powers");
+	
+	try {
+		BufferedReader br = new BufferedReader(new FileReader("input.csv"));
+		//loop through each line in csv and add values for each row
+		while ((line = br.readLine()) != null) {
+			String[] row = line.split(",");
+			//instantiate put class, add values into put instance
+			Put p = Put(Bytes.toBytes(row[0]));
+			//add values with add(column family name, qualifier/row name, value)
+			p.add(Bytes.toBytes("personal"), Bytes.toBytes("hero"), Bytes.toBytes(row[1]));
+			p.add(Bytes.toBytes("personal"), Bytes.toBytes("power"), Bytes.toBytes(row[2]));
+			p.add(Bytes.toBytes("professional"), Bytes.toBytes("name"), Bytes.toBytes(row[3]));
+			p.add(Bytes.toBytes("professional"), Bytes.toBytes("xp"), Bytes.toBytes(row[4]));
+			p.add(Bytes.toBytes("custom"), Bytes.toBytes("color"), Bytes.toBytes(row[5]));
+			
+			hTable.put(new Put(p));
+		}
+		hTable.close();
+	} catch (FileNotFoundException e) {
+		hTable.close();
+	} catch (IOException e) {
+		hTable.close();
+	}			       
    }
 }
 
